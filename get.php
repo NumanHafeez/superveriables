1d0e46ae6a36d94dbffd1abab9195189f4265e32
Numan\NumanGoraya <numanhfz10@gmail.com>
2018-01-18 09:14:13 +0500

    get super globel

---
+++ get.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<?php 
+echo "Study " . $_GET['Name'] . " Age " . $_GET['Age'];
+?>
+
+</body>
+</html>
+
+
+
+
+https://www.incapsula.com/blog/top-10-cloud-security-concerns.html
